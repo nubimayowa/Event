cba39367b914d4418d6e2d273dcadf2bdcca5248
Mayowa <nubi_mayowa@yahoo.com>
2020-11-24 12:43:02 +0100

    event app

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import { authGuard } from "../auth/authGuard";
+// import EventSingle from '../components/EventSingle.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +19,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/event/:id',
+    name: 'eventSingle',
+    component: () => import('../components/EventSingle.vue'),
+    beforeEnter : authGuard
   }
 ]
 
